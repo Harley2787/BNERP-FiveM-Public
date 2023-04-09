be5e37f2bd6c17e038002e7919f83084a5d8d78d
Harley <83428883+Harley2787@users.noreply.github.com>
2023-04-09 19:46:16 +0100

    Needs LUA Script Then Done :D

---
+++ VAR-SCREEN/fxmanifest.lua
@@ -0,0 +1,19 @@
+game 'gta5'
+author 'HarleyB'
+
+version '1.0.0'
+
+description 'The VAR Screen As a Joke'
+
+client_scripts {
+    'client/*.lua'
+}
+
+server_scripts {
+    'server/*.lua'
+}
+
+files {
+    'html/index.html',
+    'html/*.png'
+}
